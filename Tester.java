3ce0518c8d2fa51aff6c0395b65f5a0cc6236e04
J3rry-L <jliang21@stuy.edu>
2020-10-22 13:23:14 -0400

    Tested part b, largest method

--- Tester.java
+++ Tester.java
@@ -1,6 +1,15 @@
 public class Tester{
   public static void main (String[] args){
     int[] arr1 = {1,4,5,-19,60};
+    int[] arr2 = {1000,20,11,19};
+    int[] arr3 = {-85};
+    int[] empty = {};
+    System.out.println(ArrayOps.sum(empty));
     System.out.println(ArrayOps.sum(arr1));
+    System.out.println(ArrayOps.sum(arr2));
+    System.out.println(ArrayOps.sum(arr3));
+    System.out.println(ArrayOps.largest(arr1));
+    System.out.println(ArrayOps.largest(arr2));
+    System.out.println(ArrayOps.largest(arr3));
   }
 }
